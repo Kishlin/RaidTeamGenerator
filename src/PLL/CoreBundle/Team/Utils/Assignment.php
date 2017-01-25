c66c04849c5c902a7529c8da9ca3378eb290f543
Kishlin <legrand.pierrelouis@gmail.com>
2017-01-25 18:27:52 +0100

    Assignment class.

--- src/PLL/CoreBundle/Team/Utils/Assignment.php
+++ src/PLL/CoreBundle/Team/Utils/Assignment.php
@@ -2,9 +2,76 @@
 
 namespace PLL\CoreBundle\Team\Utils;
 
+use PLL\CoreBundle\Entity\Player;
+
 /**
  * Assignment
  */
 class Assignment
 {
+	/**
+	 * @var \PLL\CoreBundle\Entity\Player
+	 */
+	private $player;
+
+	/**
+	 * @var boolean
+	 */
+	private $assigned;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->player   = null;
+		$this->assigned = false;
+	}
+
+	/**
+	 * Assign a player
+	 * 
+	 * @param  Player     $player 
+	 * @return Assignment this
+	 */
+	public function assign(Player $player)
+	{
+		$this->assigned = true;
+		$this->player   = $player;
+
+		return $this;
+	}
+
+	/**
+	 * Undo the assignment
+	 * 
+	 * @return Assignment this
+	 */
+	public function unassign()
+	{
+		$this->player   = null;
+		$this->assigned = false;
+
+		return $this;
+	}
+
+	/**
+	 * Returns wether the assignment was made.
+	 * 
+	 * @return boolean assigned
+	 */
+	public function isAssigned()
+	{
+		return $this->assigned;
+	}
+
+	/**
+	 * Returns the assigned player.
+	 * 
+	 * @return \PLL\CoreBundle\Entity\Player player
+	 */
+	public function getPlayer()
+	{
+		return $this->player;
+	}
 }
--- src/PLL/CoreBundle/Team/Utils/Team.php
+++ src/PLL/CoreBundle/Team/Utils/Team.php
@@ -2,9 +2,39 @@
 
 namespace PLL\CoreBundle\Team\Utils;
 
+
+use PLL\CoreBundle\Entity\Composition;
+use PLL\CoreBundle\Entity\Player;
+use PLL\CoreBundle\Entity\Build;
+
 /**
  * Team
  */
 class Team
 {
+	/**
+	 * @var PLL\CoreBundle\Entity\Composition
+	 */
+	private $composition;
+
+	/**
+	 * @var array
+	 */
+	private $assignments;
+
+	public function __construct(Composition $composition)
+	{
+		$this->composition = $composition;
+		$this->assignments = array();
+
+		for ($i=0, $len = $composition->getSize(); $i < $len; $i++) { 
+			$this->assignments[$i] = new Assignment();
+		}
+	}
+
+	public function assign(Player $player, Build $build)
+	{
+		
+	}
+
 }
